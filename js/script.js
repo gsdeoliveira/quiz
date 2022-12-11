490e5e68fa3853ee7366c6ddc4ba9a7e5b4cfcc7
gsdeoliveira <gabriielsousa2002@gmail.com>
2022-12-10 22:05:10 -0300

    commit first question

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var container = document.getElementById('container')
+var perguntas = document.getElementById('perguntas')
+
+function botaoIniciar() {
+    container.style.display = 'none'
+    perguntas.classList.add('animacao-pergunta')
+    perguntas.style.display = 'block'
+}
+
+function verificaOpcao() {
+
+}
